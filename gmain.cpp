0e992d4570d3e034d816a680e5e097d9ff8e5ebb
Coding-Ocean <eijo524@gmail.com>
2025-01-17 14:30:42 +0900

    ref

--- gmain.cpp
+++ gmain.cpp
@@ -10,9 +10,7 @@ void gmain()
 	int n = 5;
 	int inc = 1;
 	float toRad = 3.1415926f / 180;
-	float elapsed = 0;
-	UINT64 cnt = 0;
-	float px = width / 2;
+	float px = width / 4 * 3;
 	float py = height / 2;
 	float dx = -400;
 	initDeltaTime();
@@ -22,9 +20,6 @@ void gmain()
 		setDeltaTime();
 		//1秒間に回転する角度をラジアンに変換
 		rad += 90.0f * toRad * delta;
-		//経過時間
-		elapsed += delta;
-		cnt = cnt + 1;
 		//指定秒ごとにｎを変更
 		if (timer(1.0f)) {
 			if (n < 1 || n > 4) {
@@ -35,35 +30,36 @@ void gmain()
 
 		//描画------------------------------------------------
 		beginMsaaRender();
-		
+
 		//rect, circle
-		//stroke(0, 0, 0);
-		//strokeWeight(5);
-		//for (int y = 0; y < n; ++y) {
-		//	for (int x = 0; x < n; ++x) {
-		//		if ((x + y) % 2) {
-		//			fill(1, 1, 1, 0);
-		//		}
-		//		else {
-		//			fill(1, 1, 1, 1);
-		//		}
-		//		float w = 80.0f;
-		//		float h = 80.0f;
-		//		float ofstX = (width - w * n) / 2 + w / 2;
-		//		float ofstY = (height - h * n) / 2 + h / 2;
-		//		float px = ofstX + w * x;
-		//		float py = ofstY + h * y;
-		//		rect(px, py, w, h, rad);
-		//		fill(1, 1, 0.4f);
-		//		circle(px, py, w * 0.5f);
-		//	}
-		//}
+		stroke(0, 0, 0);
+		strokeWeight(5);
+		for (int y = 0; y < n; ++y) {
+			for (int x = 0; x < n; ++x) {
+				if ((x + y) % 2) {
+					fill(1, 1, 1, 0);
+				}
+				else {
+					fill(1, 1, 1, 1);
+				}
+				float w = 80.0f;
+				float h = 80.0f;
+				float ofstX = (width - w * n) / 2 + w / 2;
+				float ofstY = (height - h * n) / 2 + h / 2;
+				float px = ofstX + w * x;
+				float py = ofstY + h * y;
+				rect(px, py, w, h, rad);
+				fill(1, 1, 0.4f);
+				circle(px, py, w * 0.5f);
+			}
+		}
 
 		//image ファイル名重複しても大丈夫です
 		if (px<0 && dx<0)dx *= -1;
 		if (px>width && dx>0)dx *= -1;
-		px += dx*delta;
-		image("assets/penguin1.png",px, py, 0, 0.3f, 0.3f);
+		px += dx * delta;
+		imageColor(1.0f, 0.5f, 0.5f);
+		image("assets/penguin1.png",px, py, 0, 0.5f, 0.5f);
 
 		//arrow
 		float sx = width / 6;
@@ -79,22 +75,11 @@ void gmain()
 		fontColor(0.f, 0.f, 0.f);
 		text("a", ex + cosf(rad) * 22, ey - sinf(rad) * 22);
 
-		//text 漢字
-		int size = 100;
-		fontSize(size);
-		fontColor(0.f, 0.6f, 0.f);
-		fontRectModeCorner();
-		std::string str = "色即是空、空即是色";//←漢字にしてください
-		text(str.c_str(), (width - size * 0.5f * str.size()) / 2, (height - size));
-
 		//print
 		fontSize(50);
 		fontColor(0.f, 0.f, 0.f);
-		print("numLoadTextures:%u", numLoadTextures());
-		print("numFontTextures:%u", numFontTextures());
-		print("numConstants:%u", numConstants());
-		print("elapsed/cnt:%f", elapsed/cnt);
-		print("delta:      %f", delta);
+		fontRectModeCorner();
+		print("delta:%.3f", delta);
 		endMsaaRender();
 	}
 }
