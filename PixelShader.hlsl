91d41d0d15d56cdadee9ae1801e745cbb496be86
Coding-Ocean <eijo524@gmail.com>
2025-03-18 10:32:01 +0900

    1_09

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -1,32 +1,40 @@
 #include<Header.hlsli>
 
-float3 palette(float t)
+float hit_sphere(const float3 center, float radius, const float3 rayDirection)
 {
-    float3 a = float3(0.5, 0.5, 0.5);
-    float3 b = float3(0.5, 0.5, 0.5);
-    float3 c = float3(1, 1, 1);
-    float3 d = float3(0.263, 0.416, 0.557);
+    float3 oc = RayOrigin - center;
+    float a = dot(rayDirection, rayDirection);
+    float b = 2.0 * dot(oc, rayDirection);
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}
 
-    return a + b * cos(6.28318 * (c * t + d));
+float4 ray_color(float3 rayDirection)
+{
+    float3 center = float3(0, 0, -1);
+    float t = hit_sphere(center, 0.5, rayDirection);
+    if (t > 0.0)
+    {
+        float3 N = normalize(rayDirection*t - center);
+        return float4(0.5 * float3(N.x + 1, N.y + 1, N.z + 1), 1);
+    }
+    //background
+    float3 unit_direction = normalize(rayDirection);
+    t = 0.5 * (unit_direction.y + 1.0);
+    float3 color = (1.0 - t) * float3(0.5, 0.7, 1.0) + t * float3(1.0, 1.0, 1.0);
+    return float4(color, 1);
 }
 
 float4 main(float4 i_pos : SV_POSITION, float2 i_uv : TEXCOORD) : SV_TARGET
 {
-    float2 uv = (i_uv - 0.5)*3.5;
-    float2 uv0 = uv;
-    float3 finalCol = float3(0, 0, 0);
-    for (int i = 0; i < 4; ++i)
-    {
-        uv *= 1.5;
-        uv = uv - floor(uv);
-        uv -= 0.5f;
-    
-        float d = length(uv) * exp(-length(uv0));
-        float3 col = palette(length(uv0) + Time * 0.2);
-        d = sin(d * 8 + Time*0.2);
-        d = abs(d);
-        d = pow(0.04 / d, 2);
-        finalCol += col * d;
-    }
-    return float4(finalCol, 1);
+    float3 rayDirection = float3(i_uv, -1) - RayOrigin;
+    return ray_color(rayDirection);
 }
--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,94 @@
 #include"graphic.h"
 
+class vec3 {
+public:
+	vec3() : e{ 0,0,0 } {}
+	vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+
+	double x() const { return e[0]; }
+	double y() const { return e[1]; }
+	double z() const { return e[2]; }
+
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	double operator[](int i) const { return e[i]; }
+	double& operator[](int i) { return e[i]; }
+
+	vec3& operator+=(const vec3& v) {
+		e[0] += v.e[0];
+		e[1] += v.e[1];
+		e[2] += v.e[2];
+		return *this;
+	}
+
+	vec3& operator*=(const double t) {
+		e[0] *= t;
+		e[1] *= t;
+		e[2] *= t;
+		return *this;
+	}
+
+	vec3& operator/=(const double t) {
+		return *this *= 1 / t;
+	}
+
+	double length() const {
+		return sqrt(length_squared());
+	}
+
+	double length_squared() const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+
+public:
+	double e[3];
+};
+
+// vec3 ユーティリティ関数
+inline vec3 operator+(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+	return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+	return (1 / t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+	return u.e[0] * v.e[0]
+		+ u.e[1] * v.e[1]
+		+ u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+		u.e[2] * v.e[0] - u.e[0] * v.e[2],
+		u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
+
+// vec3 の型エイリアス
+using point3 = vec3;   // 3D 点
+using color = vec3;    // RGB 色
+
+
 //頂点バッファ
 ComPtr<ID3D12Resource>   VertexBuffer;
 D3D12_VERTEX_BUFFER_VIEW Vbv;
@@ -9,23 +98,26 @@ UINT CbvIdx;
 //コンスタントバッファ０構造体。Header.hlsliと同じ並びにしておく
 struct CONST_BUF0 {
 	float time;
+	float rayOrigin[3];
 }* CB0;
 
 //Entry point
 INT WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ INT)
 {
-	window(L"Shader Toy", 1920, 1080, false);
+	window(L"RayTracing", 1920/2, 1080/2, WINDOW);
+	//window(L"Shader Toy", 1080, 1080, WINDOW);
 
 	//リソース初期化
 	{
 		//頂点バッファ
 		{
+			//texcoordは、xを-aspect ~ aspect,　yを-1~1とする
 			float vertices[] = {
-				//position                       texcoord
-				-1.0f,  1.0f * aspect(),  0.0f,  0.0f,  0.0f, //左上
-				-1.0f, -1.0f * aspect(),  0.0f,  0.0f,  1.0f, //左下
-				 1.0f,  1.0f * aspect(),  0.0f,  1.0f,  0.0f, //右上
-				 1.0f, -1.0f * aspect(),  0.0f,  1.0f,  1.0f, //右下
+				//position            texcoord
+				-1.0f,  1.0f,  0.0f,  -aspect(),  -1.0f, //左上
+				-1.0f, -1.0f,  0.0f,  -aspect(),  1.0f, //左下
+				 1.0f,  1.0f,  0.0f,  aspect(),  -1.0f, //右上
+				 1.0f, -1.0f,  0.0f,  aspect(),  1.0f, //右下
 			};
 			unsigned numVertexElements = 5;
 			//データサイズを求めておく
@@ -51,12 +143,23 @@ INT WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ INT)
 		}
 	}
 
+	//auto aspect_ratio = aspect();
+	//auto viewport_height = 2.0;
+	//auto viewport_width = aspect_ratio * viewport_height;
+	//auto focal_length = 1.0;
+	//auto origin = point3(0, 0, 0);
+	//auto horizontal = vec3(viewport_width, 0, 0);
+	//auto vertical = vec3(0, viewport_height, 0);
+	//auto lower_left_corner =
+	//	origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
 	timeBeginPeriod(1);
 	DWORD startTime = timeGetTime();
 	ShowCursor(false);
 	while (!quit())
 	{
 		CB0->time = (timeGetTime() - startTime) / 1000.0f;
+		CB0->rayOrigin[0] = 0; CB0->rayOrigin[1] = 0; CB0->rayOrigin[2] = 0;
 		beginRender();
 		drawMesh(Vbv, CbvIdx);
 		endRender();
