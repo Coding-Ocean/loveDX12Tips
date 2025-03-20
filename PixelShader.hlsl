94c12501cc9fae9da8ae0951221f278f814468ee
Coding-Ocean <eijo524@gmail.com>
2025-03-20 11:50:31 +0900

    box,smin

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -1,22 +1,29 @@
 #include<Header.hlsli>
 
-#define MAX_STEPS 100
-#define MAX_DIST 200.0
-#define SURF_DIST 0.01
-
+#define MAX_STEPS 80
+#define MAX_DIST 100.0
+#define SURF_DIST 0.001
+float smin(float a, float b, float k)
+{
+    float h = max(k - abs(a - b), 0.) / k;
+    return min(a, b) - h * h * h * k * (1. / 6.);
+}
 float GetDist(float3 p)
 {
     //sphere
-    float4 s = float4(0, 1, 5, 1);//x, y, z, radius
-    float sphereDist = length(p - s.xyz) - s.w;
+    float3 spherePos = float3(sin(Time)*2.2, 1, 5);//x, y, z, radius
+    float sphere = length(p - spherePos) - 1.;
+    
+    //box
+    float3 boxPos = float3(0, 1, 5);
+    float3 q = abs(p-boxPos) - .7;
+    float box = length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0);
     
     //plane
-    float planeDist = p.y;
+    float plane = p.y;
     
-    float dist = min(sphereDist, planeDist);
-    return dist;
+    return min(plane, smin(box, sphere, 1.));
 }
-
 float RayMarch(float3 ro, float3 rd)
 {
     float dO = 0.;
@@ -49,8 +56,8 @@ float3 GetNormal(float3 p)
 
 float Lighting(float3 p)
 {
-    float3 lightPos = float3(0, 6, 3);
-    lightPos.xz += float2(sin(Time), cos(Time)) * 2;
+    float3 lightPos = float3(0, 3, 2);
+    //lightPos.xz += float2(sin(Time), cos(Time)) * 2;
     
     float3 l = normalize(lightPos - p);
     float3 n = GetNormal(p);
@@ -58,7 +65,7 @@ float Lighting(float3 p)
     float diffuse = clamp(dot(n, l), 0., 1.);
     float d = RayMarch(p + n * SURF_DIST * 2., l);
     if (d < length(lightPos - p))
-        diffuse *= .1;//‰e‚È‚Ì‚ÅˆÃ‚­‚·‚é
+        diffuse *= .5;//‰e‚È‚Ì‚ÅˆÃ‚­‚·‚é
     
     return diffuse;
 }
@@ -66,16 +73,16 @@ float Lighting(float3 p)
 float4 main(float4 i_pos : SV_POSITION, float2 i_uv : TEXCOORD) : SV_TARGET
 {
     float3 ro = float3(0, 1, 0);//ray origin
-    float3 rd = normalize(float3(i_uv, 1));//ray direction
+    float3 rd = normalize(float3(i_uv, 1.));//ray direction
+    float3 col = 0;//final color
 
     float d = RayMarch(ro, rd);
     
-    float3 col = 0;
     //col = 1 - d / 10; //‚„‚Ì’lŽ‹Šo‰»
     //return float4(col, 1);
     
     float3 p = ro + rd * d;
     float diffuse = Lighting(p);
-    col = pow(diffuse, 0.4545); //gamma correction
+    col = pow(diffuse, 1); //gamma correction
     return float4(col, 1);
 }
