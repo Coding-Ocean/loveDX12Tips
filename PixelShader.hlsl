813a720eeadec612b03dcd5deccca626e12a050c
Coding-Ocean <eijo524@gmail.com>
2025-03-21 16:15:58 +0900

    ref

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -3,6 +3,7 @@
 #define MAX_STEPS 80
 #define MAX_DIST 100.0
 #define SURF_DIST 0.001
+
 float2x2 rot2D(float t)
 {
     return float2x2(cos(t), -sin(t), sin(t), cos(t));
@@ -15,53 +16,51 @@ float smin(float a, float b, float k)
 float GetDist(float3 p)
 {
     //sphere
-    float3 spherePos = float3(cos(Time*0.3)*1.2, 1, 0);
+    float3 spherePos = float3(cos(Time * 0.3) * 2.5, 1, 0);
     float sphere = length(p - spherePos) - 1.;
-    //float sphere2 = length(p - float3(-cos(Time*0.3)*1.2, 1, 0)) - 1.;
+    //spherePos.x *= -1;
+    //float sphere2 = length(p - spherePos) - 1.;
     
     //box
-    float3 p_ = p;
+    float3 p_ = p;//copy
     p_.y -= 1;
     p_.xz = mul(p_.xz, rot2D(Time*2));
     p_.xy = mul(p_.xy, rot2D(Time*2));
-    float3 q = abs(p_) - .5;
+    float3 q = abs(p_) - .6;
     float box = length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0);
     
     //plane
-    float plane = p.y+1.;
+    float plane = p.y + 1.;
     
     //return min(plane, smin(sphere, sphere2, 1.));
+    //return min(plane, box);
     return min(plane, smin(sphere, box, 1.));
 }
 float RayMarch(float3 ro, float3 rd)
 {
-    float dO = 0.;
-    
+    float t = 0.;
     for (int i = 0; i < MAX_STEPS; i++)
     {
-        float3 p = ro + rd * dO;
-        float dS = GetDist(p);
-        dO += dS;
-        if (dO > MAX_DIST || dS < SURF_DIST)
+        float3 p = ro + rd * t;
+        float d = GetDist(p);
+        t += d;
+        if (t > MAX_DIST || d < SURF_DIST)
             break;
     }
-    
-    return dO;
+    return t;
 }
-
 float3 GetNormal(float3 p)
 {
-    float d = GetDist(p);
+    float t = GetDist(p);
     float2 e = float2(0.01, 0);
     
-    float3 n = d - float3(
+    float3 n = t - float3(
         GetDist(p - e.xyy),
         GetDist(p - e.yxy),
         GetDist(p - e.yyx));
     
     return normalize(n);
 }
-
 float Lighting(float3 p)
 {
     float3 lightPos = float3(0, 3, -2);
@@ -70,27 +69,29 @@ float Lighting(float3 p)
     float3 l = normalize(lightPos - p);
     float3 n = GetNormal(p);
     
-    float diffuse = clamp(dot(n, l), 0., 1.);
+    float bright = clamp(dot(n, l), 0., 1.);
     float d = RayMarch(p + n * SURF_DIST * 2., l);
     if (d < length(lightPos - p))
-        diffuse *= .5;//影なので暗くする
+        bright *= .5;//影なので暗くする
     
-    return diffuse;
+    return bright;
 }
-
 float4 main(float4 i_pos : SV_POSITION, float2 i_uv : TEXCOORD) : SV_TARGET
 {
     float3 ro = float3(0, 1, -5);//ray origin
     float3 rd = normalize(float3(i_uv, 1.));//ray direction
     float3 col = 0;//final color
 
-    float d = RayMarch(ro, rd);
+    float t = RayMarch(ro, rd);//tはオブジェクト表面までの距離
     
-    //col = 1 - d / 10; //ｄの値視覚化
+    //col = 1 - t / 10; //tの値視覚化
     //return float4(col, 1);
     
-    float3 p = ro + rd * d;
-    float diffuse = Lighting(p);
-    col = pow(diffuse, 1); //gamma correction
+    float3 p = ro + rd * t;
+    float bright = Lighting(p);
+    col = bright;
+    return float4(col, 1);
+    
+    col = pow(bright, .4545); //gamma correction
     return float4(col, 1);
 }
